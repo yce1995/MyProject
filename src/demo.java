7f91c4265d320c4bf28cd053121b9f1fde6b4db9
yce1995 <1058097131@qq.com>
2017-12-28 19:40:28 +0800

    1

---
+++ src/demo.java
@@ -0,0 +1,8 @@
+
+public class demo {
+	public static void main(String[] args) {
+		System.out.println("(๑╹◡╹)ﾉ”，我系渣渣辉");
+		System.out.println("(๑╹◡╹)ﾉ”，我系渣渣辉");
+		System.out.println("(๑╹◡╹)ﾉ”，我系渣渣辉");
+	}
+}
