9b2d30e75cb73831a3ea0fe092b3ea7119a6705e
yce1995 <34907659+yce1995@users.noreply.github.com>
2017-12-29 16:27:51 +0800

    Update demo.java

--- src/demo.java
+++ src/demo.java
@@ -4,6 +4,7 @@ public class demo {
 		System.out.println("(๑╹◡╹)ﾉ”，我系渣渣辉");
 		System.out.println("(๑╹◡╹)ﾉ”，我系渣渣辉");
 		System.out.println("(๑╹◡╹)ﾉ”，我系渣渣辉");
-		
+		System.out.println("(๑╹◡╹)ﾉ”，我系渣渣辉");
+		System.out.println("(๑╹◡╹)ﾉ”，我系渣渣强");
 	}
 }
